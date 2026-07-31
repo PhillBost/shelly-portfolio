235219f3bd279766a2c7849d9dceca84147b49bb
Phillip B <pbostic789@gmail.com>
2026-07-31 14:15:52 -0400

    updated visuals

---
+++ src/content/siteContent.js
@@ -0,0 +1,19 @@
+export const siteContent = {
+  bio: `
+    My love for painting the face arose after being a makeup artist for
+    12 years. I discovered that the elements of the face create a painting
+    through the colors of the skin, eyes, lips, and hair.
+
+    These features, their colors, along with the makeup used alongside them,
+    can create many different feelings on one subject. I’ve been using oils
+    and acrylic as a medium for 8 years, and each painting begins with a
+    color story in mind before I have chosen a subject.
+
+    Recently, I’ve had an interest in painting vases and flowers, using the
+    palette knife to create unique textures in each piece.
+  `,
+
+  aboutTitle: "About Me",
+
+  galleryTitle: "Gallery",
+};
